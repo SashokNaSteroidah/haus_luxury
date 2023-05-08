bdd17b3f3e11aede79f03bd0568f691f2283c85a
letspl <pavliushin.nikita@mail.ru>
2023-05-08 22:12:44 +0300

    main full

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
-document.querySelector('#signin').onclick = () => {
-    document.querySelector('.back_black').style.display = 'block'
-    document.querySelector('.signin').style.display = 'block'
-}
+let scrollpos = window.scrollY
 
-document.querySelector('#signup').onclick = () => {
-    document.querySelector('.back_black').style.display = 'block'
-    document.querySelector('.signup').style.display = 'block'
-}
+const header = document.querySelector("header")
+const scrollChange = 300
 
-document.querySelector('.back_black').onclick = () => {
-    document.querySelector('.back_black').style.display = 'none'
-    document.querySelector('.signin').style.display = 'none'
-    document.querySelector('.signup').style.display = 'none'
-}
+const add_class_on_scroll = () => header.classList.add("header_change_main")
+const remove_class_on_scroll = () => header.classList.remove("header_change_main")
+
+window.addEventListener('scroll', function() {
+    scrollpos = window.scrollY;
+
+    if (scrollpos >= scrollChange) { add_class_on_scroll() }
+    else { remove_class_on_scroll() }
+
+})
+
+// document.querySelector('#signin').onclick = () => {
+//     document.querySelector('.back_black').style.display = 'block'
+//     document.querySelector('.signin').style.display = 'block'
+// }
+//
+// document.querySelector('#signup').onclick = () => {
+//     document.querySelector('.back_black').style.display = 'block'
+//     document.querySelector('.signup').style.display = 'block'
+// }
+//
+// document.querySelector('.back_black').onclick = () => {
+//     document.querySelector('.back_black').style.display = 'none'
+//     document.querySelector('.signin').style.display = 'none'
+//     document.querySelector('.signup').style.display = 'none'
+// }
